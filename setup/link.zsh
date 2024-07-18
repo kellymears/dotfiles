d87911d925de41d8610748242735596342c428b6
Kelly Mears <developers@tinypixel.dev>
2024-07-18 13:08:31 -0400

    .hushlogin

--- setup/link.zsh
+++ setup/link.zsh
@@ -5,6 +5,7 @@ ln -s $dotfiles_dir/.zshrc                         $HOME/.zshrc
 ln -s $dotfiles_dir/config/.ackrc                  $HOME/.ackrc
 ln -s $dotfiles_dir/config/.gitconfig              $HOME/.gitconfig
 ln -s $dotfiles_dir/config/.gitignore              $HOME/.gitignore
+ln -s $dotfiles_dir/config/.hushlogin              $HOME/.hushlogin
 ln -s $dotfiles_dir/config/.skhdrc                 $HOME/.skhdrc
 ln -s $dotfiles_dir/config/.yabairc                $HOME/.yabairc
 ln -s $dotfiles_dir/config/vscode/settings.json    $vscode_user_dir/settings.json
